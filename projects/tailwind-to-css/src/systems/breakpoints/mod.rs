7f515743dffa337821a98073ff7ff1069c5a57a9
Aster <galaster@foxmail.com>
2021-04-06 18:28:56 +0800

    🐣 Add BreakPointSystem

--- projects/tailwind-to-css/src/systems/breakpoints/mod.rs
+++ projects/tailwind-to-css/src/systems/breakpoints/mod.rs
@@ -1,19 +1,27 @@
+use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 
 pub struct BreakPointSystem {
+    inner: HashMap<String, BreakPoint>,
+}
 
+impl BreakPointSystem {
+    #[inline]
+    pub fn register(&mut self, name: String, width: usize) -> Option<BreakPoint> {
+        self.inner.insert(name, BreakPoint {
+            width
+        })
+    }
 }
 
 /// sm	640px	@media (min-width: 640px) { ... }
 pub struct BreakPoint {
-    name: String,
     /// min-width
     /// unit: px
-    width: usize
+    width: usize,
 }
 
 impl Display for BreakPoint {
-
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         todo!()
     }
